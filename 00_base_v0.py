b02a7bb57a77411a3167b4f1da290fe1313484a5
abbyive <107890625+abbyive@users.noreply.github.com>
2022-06-21 09:25:41 +1200

    skeleton of base code
    version 0

---
+++ 00_base_v0.py
@@ -0,0 +1,18 @@
+"""
+
+base component for the game Lucky Unicorn
+V0 - skeleton setup
+
+author - Abby Ives
+CC AVI 2022
+
+"""
+
+# import libraries below this line ****************************************
+
+
+# all functions below this line ********************************************
+
+
+# print main routine below this line *************************************
+print("hello")
