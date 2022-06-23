d78decf116f655f0973bd8e7ecdded1904624df1
abbyive <107890625+abbyive@users.noreply.github.com>
2022-06-23 13:02:24 +1200

    more efficient yes/no checker
    second version, including 'or' operator to simplify code

---
+++ 01_yes_no_v2.py
@@ -0,0 +1,19 @@
+show_instructions = ""
+while show_instructions.lower() != "xxx":
+
+    # ask the user if they have played before
+    show_instructions = input("Have you played this game before? ").lower()
+
+    # if they say yes, output 'program continues'
+    # if they say anything else, send 'error'
+    # if they say no, output 'display instructions'
+    if show_instructions == "yes" or show_instructions == "y":
+        show_instructions = "yes"
+        print("program continues")
+
+    elif show_instructions == "no" or show_instructions == "n":
+        show_instructions = "no"
+        print("display instructions")
+
+    else:
+        print("Please enter either 'Yes' or 'No'.")
